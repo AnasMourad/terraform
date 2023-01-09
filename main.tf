2bf3c032484a9f3291e17ed615a20bf4f6f13c21
AnasMourad <anasmouradb2a@gmail.com>
2023-01-09 15:32:18 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,11 @@ terraform{
 
   cloud{
     orgainization = "terraform-cloud-organization"
+    workspaces{
+      name = "common"
+    }
   }
-  workspaces{
-    name = "common"
-  }
+  
 
 }
 resource "random_pet" "vpc" {}
